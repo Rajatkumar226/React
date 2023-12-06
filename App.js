0e35bcb3a8e50a238ba8c4a453e3bceb76c99c8b
Rajat Kumar <rajat8615226@gmail.com>
2023-12-06 21:57:16 +0530

    trying to connect git with vscode

---
+++ App.js
@@ -0,0 +1,14 @@
+// const heading = React.createElement("div", { id: "rjt" }, [
+//   React.createElement("h1", { id: "rjt1" }, "Hello from Rajat"),
+//   React.createElement("h2", { id: "rjt2" }, "hey bro"),
+// ]);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+    //  <script>
+    // const heading = document.createElement("h1");
+    // heading.innerHTML = "Hey Rajat from JavaScript";
+    
+    // const root = document.getElementById("root");
+    // root.appendChild(heading);
+    //     </script> 
